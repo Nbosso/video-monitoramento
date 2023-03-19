c5083ec70159a4b2079b92e08698230699bb7b79
Natan Santos Bosso <bosso.natan@chavi.com.br>
2023-03-19 15:56:14 -0300

    feat: create module login

---
+++ app/lib/app/app_module.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'modules/login/login_module.dart';
+
+// import 'app_store.dart';
+
+class AppModule extends Module {
+  @override
+  final List<Bind> binds = [
+    // Bind.lazySingleton((i) => AppStore()),
+  ];
+
+  @override
+  final List<ModularRoute> routes = [
+            ModuleRoute('/login', module: LoginModule()),
+  ];
+}
---
+++ app/lib/app/app_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+// import 'app_store.dart';
+
+class AppPage extends StatefulWidget {
+  final String title;
+  const AppPage({Key? key, this.title = 'AppPage'}) : super(key: key);
+  @override
+  AppPageState createState() => AppPageState();
+}
+class AppPageState extends State<AppPage> {
+  // final AppStore store = Modular.get();
+
+  @override
+  Widget build(BuildContext context) {
+    Modular.setInitialRoute('/login/');
+
+    return MaterialApp.router(
+      title:  'App Video Monitoramento',
+      debugShowCheckedModeBanner: false,
+      routerDelegate: Modular.routerDelegate,
+      routeInformationParser: Modular.routeInformationParser,
+    );
+  }
+}
